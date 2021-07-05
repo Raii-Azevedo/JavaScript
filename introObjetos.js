3a867bce2d74437294e930376b9fc8a89e2dc605
Raissa Azevedo <rhaii.azevedo@gmail.com>
2021-07-05 15:54:32 -0300

    Intro a objetos

---
+++ introObjetos.js
@@ -0,0 +1,12 @@
+const pessoa = {nome: 'Raíssa', idade: 28};
+
+console.log(pessoa.nome);
+
+
+const quadrado = {
+    base: 10,
+    altura: 20,
+    calculaArea: function(){return this.base*this.altura}
+};
+
+console.log(quadrado.calculaArea());
