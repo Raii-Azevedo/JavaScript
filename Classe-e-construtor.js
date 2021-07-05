cc4cbb5e3a01ba1a8300660771f430a746d8c328
Raissa Azevedo <rhaii.azevedo@gmail.com>
2021-07-05 18:49:49 -0300

    Classe e Construtor

---
+++ Classe-e-construtor.js
@@ -0,0 +1,12 @@
+class Pessoa {
+    constructor (nome, idade){
+        this.nome = nome;
+        this.idade = idade;
+    }
+};
+
+const pessoa1 = new Pessoa('Raissa', 28);
+console.log(pessoa1);
+
+const pessoa2 = new Pessoa('Pedro', 23);
+console.log(pessoa2);
